0e20e960478900c3a20b626b9e93ec67c687b8b4
Matt Kim <mingyuya@gmail.com>
2017-07-03 22:39:18 +0900

    add source codes and results

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -1,4 +1,5 @@
 #include "kalman_filter.h"
+#include <math.h>
 
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
@@ -18,22 +19,59 @@ void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
 }
 
 void KalmanFilter::Predict() {
-  /**
-  TODO:
-    * predict the state
-  */
+  x_ = F_*x_;
+  P_ = F_*P_*F_.transpose() + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
-  /**
-  TODO:
+  /*
     * update the state by using Kalman Filter equations
   */
+  VectorXd y = z - H_*x_;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_*P_*Ht + R_;
+  MatrixXd K = P_*Ht*S.inverse();
+
+  // New estimate
+  x_ = x_ + (K*y);
+
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I-K*H_)*P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
-  /**
-  TODO:
-    * update the state by using Extended Kalman Filter equations
-  */
+  float px = x_[0];
+  float py = x_[1];
+  float vx = x_[2];
+  float vy = x_[3];
+
+  // Convert cartesian to polar coordinate
+  float rho = sqrt(px*px+py*py);
+  float phi = atan2(py, px);
+  float rho_dot = (px*vx + py*vy)/rho;
+
+  VectorXd z_pred;
+  z_pred = VectorXd(3);
+  z_pred << rho, phi, rho_dot;
+
+  VectorXd y = z - z_pred;
+
+  // Adjust phi to be located between (-pi, pi)
+  if (y[1] > M_PI) {
+    y[1] = 2*M_PI - y[1];
+  } else if (y[1] < -M_PI) {
+    y[1] = 2*M_PI + y[1];
+  }
+
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_*P_*Ht + R_;
+  MatrixXd K = P_*Ht*S.inverse();
+
+  // New estimate
+  x_ = x_ + (K*y);
+
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I-K*H_)*P_;
 }
--- src/tools.cpp
+++ src/tools.cpp
@@ -12,14 +12,62 @@ Tools::~Tools() {}
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
   /**
-  TODO:
     * Calculate the RMSE here.
   */
+  VectorXd rmse(4);
+  rmse << 0,0,0,0;
+
+  // Check the validity of the inputs :
+  //  1) The estimation vector size should not be zero
+  //  2) The estimation vector size should equal ground truth vector size
+  if (estimations.size() == 0) 
+    cout << "CalculateRMSE() - Error : The estimation size is zero" << endl;
+  else if (estimations.size() != ground_truth.size())
+    cout << "CalculateRMSE() - Error : The estimation vector size is not equal to ground truth vector" << endl;
+
+  VectorXd residual(4);
+  VectorXd sum(4);
+  sum << 0,0,0,0;
+
+  // Accumulate squared residuals
+  for (int i=0; i < estimations.size() ; ++i) {
+    residual = estimations[i] - ground_truth[i];
+    residual = residual.array()*residual.array();
+    sum = sum + residual;
+  }
+  // Mean
+  rmse = sum.array()/estimations.size();
+  // Square root
+  rmse = sqrt(rmse.array());
+
+  return rmse;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
   /**
-  TODO:
-    * Calculate a Jacobian here.
+    * Calculate a Jacobian for Extended KF.
   */
+  MatrixXd Hj(3,4); // 3 for rho, phi, rho_dot
+                    // 4 for px, py, vx, vy
+  float px = x_state(0);
+  float py = x_state(1);
+  float vx = x_state(2);
+  float vy = x_state(3);
+
+  float c1 = px*px + py*py;
+  float c2 = sqrt(c1);
+  float c3 = c1*c2; 
+
+  // Check division by zero
+  if (fabs(c1) < 0.0001) {
+    cout << "CalculateJacobian() - Error : Division by Zero" << endl;
+    return Hj;
+  }
+  
+  // Calculate the Jacobian
+  Hj <<                 px/c2 ,                 py/c2 ,     0 ,     0 ,
+                     -(py/c1) ,                 px/c1 ,     0 ,     0 ,
+        py*(vx*py - vy*px)/c3 , px*(px*vy - py*vx)/c3 , px/c2 , py/c2 ;
+
+  return Hj;
 }
